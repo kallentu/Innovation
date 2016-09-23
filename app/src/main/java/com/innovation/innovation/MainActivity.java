aec1e09e455d6d2d3dfee5f856e0a8161c336ef5
Kallen Tu <kallentu@hotmail.ca>
2016-09-22 21:17:12 -0700

    New gradle and added k12innovation names with project types in database

--- app/src/main/java/com/innovation/innovation/MainActivity.java
+++ app/src/main/java/com/innovation/innovation/MainActivity.java
@@ -15,6 +15,7 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.innovation.innovation.MESSAGE";
+    private boolean isInDB;
 
     //Views changed by database
     TextView idView;
@@ -30,6 +31,136 @@ public class MainActivity extends AppCompatActivity {
         idView = (TextView) findViewById(R.id.productID);
         productBox = (EditText) findViewById(R.id.product);
         descriptionBox = (EditText) findViewById(R.id.productDescription);
+
+        if (isInDB == false) {
+            DBHandler dbHandler = new DBHandler(this, null, null, 1);
+
+            Product alternativeEducation =
+                    new Product("Alternative Education - Dispelling Myth", "Experiential learning");
+            dbHandler.addProduct(alternativeEducation);
+
+            Product frReportCard =
+                    new Product("An Innovative Primary French Immersion Report Card", "Reporting and Assessment");
+            dbHandler.addProduct(frReportCard);
+
+            Product comShift =
+                    new Product("Assessment And How We Communicate Student Learning Alongside The Shifting Curriculum", "Reporting and Assessment");
+            dbHandler.addProduct(comShift);
+
+            Product gradeReport =
+                    new Product("Assessment, Grading And Reporting Field Test", "Reporting and Assessment");
+            dbHandler.addProduct(gradeReport);
+
+            Product authSciPart =
+                    new Product("Authentic Science Partnership: Inquire, Research, Experience (ASPIRE)", "Project-based learning");
+            dbHandler.addProduct(authSciPart);
+
+            Product comStudAss =
+                    new Product("Communicating Student Assessment", "Rural");
+            dbHandler.addProduct(comStudAss);
+
+            Product comStudLearn =
+                    new Product("Communicating Student Learning", "Multi-grade Classrooms");
+            dbHandler.addProduct(comStudLearn);
+
+            Product comStudLearn2 =
+                    new Product("Communicating Student Learning Part 2", "Reporting and Assessment");
+            dbHandler.addProduct(comStudLearn2);
+
+            Product devComp =
+                    new Product("Developing competencies", "Project-based learning");
+            dbHandler.addProduct(devComp);
+
+            Product dynScience =
+                    new Product("Dynamic Science Learning Through Student Choice", "Cross-curricular");
+            dbHandler.addProduct(dynScience);
+
+            Product elemMath =
+                    new Product("Elementary Math Innovation Project", "Aboriginal learning / Cultural development");
+            dbHandler.addProduct(elemMath);
+
+            Product firstNatOutdoors =
+                    new Product("First Nations Outdoor Education Program", "Experiential learning");
+            dbHandler.addProduct(firstNatOutdoors);
+
+            Product firstPeop =
+                    new Product("First Peoples", "Aboriginal learning / Cultural development");
+            dbHandler.addProduct(firstPeop);
+
+            Product gradeless =
+                    new Product("Going Gradeless", "Cross-curricular");
+            dbHandler.addProduct(gradeless);
+
+            Product earlyLearnClass =
+                    new Product("Inquiry in the Early Learning Classroom", "N/A");
+            dbHandler.addProduct(earlyLearnClass);
+
+            Product translationToTrans =
+                    new Product("Inquiry-based, Interdisciplinary, Competency-based Graduation Structure: Moving Beyond Translation to Transformation", "Reporting and Assessment");
+            dbHandler.addProduct(translationToTrans);
+
+            Product inspirED =
+                    new Product("InspirED", "Social, emotional learning / self-regulation");
+            dbHandler.addProduct(inspirED);
+
+            Product johnSTEM =
+                    new Product("John Oliver STEM Program", "Project-based learning");
+            dbHandler.addProduct(johnSTEM);
+
+            Product learnInDepth =
+                    new Product("Learning In Depth", "Project-based learning");
+            dbHandler.addProduct(learnInDepth);
+
+            Product innovationRoom =
+                    new Product("Making Room for Innovation", "Project-based learning");
+            dbHandler.addProduct(innovationRoom);
+
+            Product makerSpace =
+                    new Product("Mobile Makerspace", "Cross-curricular");
+            dbHandler.addProduct(makerSpace);
+
+            Product persLearn =
+                    new Product("Personalized Learning", "Vulnerable / at-risk students");
+            dbHandler.addProduct(persLearn);
+
+            Product redesignAssess =
+                    new Product("Re-Designing Assessment in a Middle School", "Reporting and Assessment");
+            dbHandler.addProduct(redesignAssess);
+
+            Product canHistory =
+                    new Product("Re-Storying Canadian History: The Interdependence of Creative and Critical Thinking", "Aboriginal learning / Cultural development");
+            dbHandler.addProduct(canHistory);
+
+            Product calibrateNormal =
+                    new Product("Recalibrating to a New Normal", "Vulnerable / at-risk students");
+            dbHandler.addProduct(calibrateNormal);
+
+            Product saHali =
+                    new Product("Sa-Hali Family of Schools", "Trades / Skills / Career Development");
+            dbHandler.addProduct(saHali);
+
+            Product teachLearnStrat =
+                    new Product("Teaching and learning strategies", "Experiential learning");
+            dbHandler.addProduct(teachLearnStrat);
+
+            Product templetonSTEM =
+                    new Product("Templeton STEM Program", "Experiential learning");
+            dbHandler.addProduct(templetonSTEM);
+
+            Product inspireProj =
+                    new Product("The ins-PI-re Project", "Trades / Skills / Career Development");
+            dbHandler.addProduct(inspireProj);
+
+            Product multiGrade =
+                    new Product("The Multi-Grade Approach to Learning", "Multi-grade classrooms");
+            dbHandler.addProduct(elemMath);
+
+            Product moduleTeach =
+                    new Product("Using a Module Approach for Teaching and Learning", "Multi-grade classrooms");
+            dbHandler.addProduct(moduleTeach);
+
+            isInDB = true;
+        }
     }
 
     //Adds new Product given from the views
@@ -59,6 +190,9 @@ public class MainActivity extends AppCompatActivity {
 
             descriptionBox.setText(String.valueOf(product.getDescription()));
 
+            /*
+            Used for changing url to image and puts it to imageview
+
             //Gets file path of image and sets it to the image view
             File imgFile = new  File(String.valueOf(product.getDescription()));
 
@@ -71,6 +205,8 @@ public class MainActivity extends AppCompatActivity {
                 myImage.setImageBitmap(myBitmap);
 
             }
+
+            */
 
         } else {
             idView.setText("No Match Found");
